0f415d992fdf946cfdc4ddec2b3d292c7febec8b
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-01-21 07:09:48 +0000

    added function stubs, to prepare the buildsystem.

--- src/nglogc/flf_log.c
+++ src/nglogc/flf_log.c
@@ -5,12 +5,124 @@
 #include "config.h"
 #endif
 
+#include "flf_log.h"
+
+#include "types.h"
+
 /* =========== MODULE CONFIGURATION ======================================== */
 /* =========== DEFINES ===================================================== */
 /* =========== DATA TYPES ================================================== */
 /* =========== GLOBALS ===================================================== */
 /* =========== PRIVATE PROTOTYPES ========================================== */
 /* =========== PUBLIC FUNCTIONS ============================================ */
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logError_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      logError_t err,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logLevelError_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      logError_t err,
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_log_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logArray_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char descriptor,
+      const* uint8_t array,
+      size_t len
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logEnter_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      log_loggerType_t type,
+      const* char functionName
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logLeave_flf_(
+      const char* file,
+      int line,
+      const char* func,
+      log_loggerType_t type,
+      const* char functionName
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return er;
+}
+/*---------------------------------------------------------------------------*/
+
+
 /* =========== PRIVATE FUNCTIONS =========================================== */
 /* ========================== END OF FILE ================================== */
 
--- src/nglogc/nflf_log.c
+++ src/nglogc/nflf_log.c
@@ -5,12 +5,105 @@
 #include "config.h"
 #endif
 
+#include "nflf_log.h"
+
+#include "types.h"
+
 /* =========== MODULE CONFIGURATION ======================================== */
 /* =========== DEFINES ===================================================== */
 /* =========== DATA TYPES ================================================== */
 /* =========== GLOBALS ===================================================== */
 /* =========== PRIVATE PROTOTYPES ========================================== */
 /* =========== PUBLIC FUNCTIONS ============================================ */
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logError_nflf_(
+      logError_t err,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logLevelError_nflf_(
+      logError_t err,
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_log_nflf_(
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char formatStr,
+      ...
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logArray_nflf_(
+      log_logLevel_t level,
+      log_loggerType_t type,
+      const* char descriptor,
+      const* uint8_t array,
+      size_t len
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logEnter_nflf_(
+      log_loggerType_t type,
+      const* char functionName
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+logError_t
+logc_logLeave_nflf_(
+      log_loggerType_t type,
+      const* char functionName
+      )
+{
+   /* TODO */
+   logError_t err = LOG_ERR_OK;
+   return err;
+}
+/*---------------------------------------------------------------------------*/
+
 /* =========== PRIVATE FUNCTIONS =========================================== */
 /* ========================== END OF FILE ================================== */
 
