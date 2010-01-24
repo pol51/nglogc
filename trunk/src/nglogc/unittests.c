05031d78f17f50fa76277f8635c065a05c024f2c
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-01-24 11:40:26 +0000

    initial add.

---
+++ trunk/src/nglogc/unittests.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+#include "log.h"
+
+int main(int argc, char *argv[])
+{
+   printf("\n starting unittets...\n");
+
+
+
+   printf("\n TESTS OF LIBNGLOGC\n");
+
+   printf(" running check_initConfig\n");
+   //check_initConfig();
+   printf("  all tests passed\n");
+
+   printf("\n");
+
+   return 0;
+}
+
