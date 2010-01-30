e347f032e9e40ac111e3e97ff38e538733aa51ab
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-01-30 18:39:55 +0000

    renamed types in types.h.

--- trunk/src/nglogc/flf_log.c
+++ trunk/src/nglogc/flf_log.c
@@ -15,81 +15,81 @@
 /* =========== PUBLIC FUNCTIONS ============================================ */
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_logError_flf_(
       const char* file,
       int line,
       const char* func,
       uint16_t ident,
-      logError_t error,
+      logc_error_t error,
       const char* formatStr,
       ...
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_logLevelError_flf_(
       const char* file,
       int line,
       const char* func,
       uint16_t ident,
-      log_logLevel_t level,
-      logError_t error,
+      logc_logLevel_t level,
+      logc_error_t error,
       const char* formatStr,
       ...
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_log_flf_(
       const char* file,
       int line,
       const char* func,
       uint16_t ident,
-      log_logLevel_t level,
+      logc_logLevel_t level,
       const char* formatStr,
       ...
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_logArray_flf_(
       const char* file,
       int line,
       const char* func,
       uint16_t ident,
-      log_logLevel_t level,
+      logc_logLevel_t level,
       const char* descriptor,
       const uint8_t* array,
       size_t len
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_logEnter_flf_(
       const char* file,
       int line,
@@ -99,13 +99,13 @@ logc_logEnter_flf_(
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------*/
-logError_t
+logc_error_t
 logc_logLeave_flf_(
       const char* file,
       int line,
@@ -115,7 +115,7 @@ logc_logLeave_flf_(
       )
 {
    /* TODO */
-   logError_t err = LOG_ERR_OK;
+   logc_error_t err = LOG_ERR_OK;
    return err;
 }
 /*---------------------------------------------------------------------------*/
