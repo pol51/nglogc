47013ad0e4eb2db4cea1b33c8cf2915bff591b61
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-02-06 19:43:52 +0000

    initial add.

---
+++ src/nglogc/timestamp.c
@@ -0,0 +1,46 @@
+
+/* =========== INCLUDES ==================================================== */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "timestamp.h"
+
+#include <time.h>
+
+/* =========== MODULE CONFIGURATION ======================================== */
+/* =========== DEFINES ===================================================== */
+
+
+/* =========== DATA TYPES ================================================== */
+/* =========== GLOBALS ===================================================== */
+/* =========== PRIVATE PROTOTYPES ========================================== */
+/* =========== PUBLIC FUNCTIONS ============================================ */
+
+void
+addTimestamp(
+      char* str
+      )
+{
+   struct tm tim;
+   time_t now;
+
+   if (str != NULL) {
+      now = time(NULL);
+      tim = *(localtime(&now));
+      strftime(str, TIMEST_LENGTH, "%c", &tim);
+      str[TIMEST_LENGTH-1] = 0x20;
+   }
+}
+
+
+/* =========== PRIVATE FUNCTIONS =========================================== */
+/* ========================== END OF FILE ================================== */
+
+/*
+ * vim settings, please do not remove!
+ * vim:autoindent:
+ * vim:ts=3:sw=3:sts=3:expandtab:cindent:tw=75:formatoptions=croql:
+ * vim600:foldmethod=syntax:
+ */
---
+++ src/nglogc/timestamp.h
@@ -0,0 +1,38 @@
+
+
+#ifndef __LOGC_TIMESTAMP_H__
+#define __LOGC_TIMESTAMP_H__
+
+
+/* =========== INCLUDES ==================================================== */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "types.h"
+
+/* =========== DEFINES ===================================================== */
+
+#define TIMEST_LENGTH   25
+
+/* =========== DATA TYPES ================================================== */
+
+/* =========== PUBLIC PROTOTYPES =========================================== */
+
+/* TODO */
+void
+addTimestamp(
+      char* str
+      );
+
+#endif
+/* ========================== END OF FILE ================================== */
+
+/*
+ * vim settings, please do not remove!
+ * vim:autoindent:filetype=c:syntax=c:
+ * vim:ts=3:sw=3:sts=3:expandtab:cindent:tw=75:formatoptions=croql:
+ * vim600:foldmethod=syntax:
+ */
+
