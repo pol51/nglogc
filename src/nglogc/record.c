a9c8fd837a448eb2dcdbe47248dc133839f77840
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-02-06 10:21:03 +0000

    added private prototypes to create different records.

--- src/nglogc/record.c
+++ src/nglogc/record.c
@@ -7,22 +7,91 @@
 
 #include "record.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 /* =========== MODULE CONFIGURATION ======================================== */
 /* =========== DEFINES ===================================================== */
 /* =========== DATA TYPES ================================================== */
 /* =========== GLOBALS ===================================================== */
 /* =========== PRIVATE PROTOTYPES ========================================== */
+static char*
+clean(
+      const char* formatStr,
+      va_list vaList
+      );
+
+
+static char*
+err_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      );
+
+
+static char*
+err_tag_timestamp(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      );
+
+
+static char*
+err_timestamp_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      );
+
+
+static char*
+timestamp_err_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      );
+
 /* =========== PUBLIC FUNCTIONS ============================================ */
 /*---------------------------------------------------------------------------*/
 logc_error_t
 newErrorRecord(
       char* record,
       logc_recordType_t rtype,
+      logc_error_t error,
+      const  char* formatStr,
       va_list vaList
       )
 {
    /* TODO */
    logc_error_t err = LOG_ERR_OK;
+
+   switch (rtype) {
+      case CLEAN:
+         record = clean(formatStr, vaList);
+         break;
+      case ERR_TAG:
+         record = err_tag(error, formatStr, vaList);
+         break;
+      case ERR_TAG_TIMESTAMP:
+         record = err_tag_timestamp(error, formatStr, vaList);
+         break;
+      case ERR_TIMESTAMP_TAG:
+         record = err_timestamp_tag(error, formatStr, vaList);
+         break;
+      case TIMESTAMP_ERR_TAG:
+         record = timestamp_err_tag(error, formatStr, vaList);
+         break;
+      default:
+         break;
+   }
+
+   if (record == NULL) {
+      err = LOG_ERR_MEM;
+   }
+
+
    return err;
 }
 /*---------------------------------------------------------------------------*/
@@ -32,6 +101,7 @@ logc_error_t
 newInfoRecord(
       char* record,
       logc_recordType_t rtype,
+      const  char* formatStr,
       va_list vaList
       )
 {
@@ -42,6 +112,65 @@ newInfoRecord(
 
 /*---------------------------------------------------------------------------*/
 /* =========== PRIVATE FUNCTIONS =========================================== */
+
+static char*
+clean(
+      const char* formatStr,
+      va_list vaList
+      )
+{
+   char* record = NULL;
+   return record;
+}
+
+
+static char*
+err_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      )
+{
+   char* record = NULL;
+   return record;
+}
+
+
+static char*
+err_tag_timestamp(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      )
+{
+   char* record = NULL;
+   return record;
+}
+
+
+static char*
+err_timestamp_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      )
+{
+   char* record = NULL;
+   return record;
+}
+
+
+static char*
+timestamp_err_tag(
+      logc_error_t error,
+      const char* formatStr,
+      va_list vaList
+      )
+{
+   char* record = NULL;
+   return record;
+}
+
 /* ========================== END OF FILE ================================== */
 
 /*
