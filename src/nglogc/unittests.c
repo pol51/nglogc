4a3c1ec9ce6a2582d090be0a1b53c28ca59105f7
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-02-08 07:15:58 +0000

    added unittests to check format of logging.

--- src/nglogc/unittests.c
+++ src/nglogc/unittests.c
@@ -2,9 +2,12 @@
 
 #include "log.h"
 
+#include "timestamp.h"
+
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <string.h>
 
 
 extern int
@@ -26,34 +29,16 @@ check_logFormat(
 
 int main(int argc, char *argv[])
 {
-   //logc_error_t err = LOG_ERR_OK;
-
    printf("\n starting unittets...\n");
-
-   printf("\n TESTS OF LIBNGLOGC\n\n");
+   printf("\n TESTS OF NGLOGC\n\n");
 
    printf("  running check_logger\n");
    check_logger();
-   printf("  all tests passed\n");
+   printf("  all tests passed\n\n");
 
    printf("  running check_logFormat\n");
    check_logFormat();
-   printf("  all tests passed\n");
-#if 0
-   logc_registerLogger(0x0012, STDOUT, LOG_WARNING);
-
-   err = logc_changeLogLevel(0x0012, LOG_BASIC);
-
-   err = logc_logError(0x0012, 1, "foobar ich bin ein error");
-   printf("logError: %08x\n", err);
-
-   err = logc_logEnter(0x0013, "main()\n");
-   err = logc_logLeave(0x0013, "main()\n");
-   printf("  all tests passed\n");
-
-   check_logger();
-   printf("\n");
-#endif
+   printf("  all tests passed\n\n");
 
    return 0;
 }
@@ -110,8 +95,17 @@ check_logFormat(
       void
       )
 {
-   assert(logc_registerLogger(0x0011, STDOUT, LOG_BASIC) == LOG_ERR_OK);
+   FILE* fd = NULL;
+   char checkBuf[256] = {0};
+   char tmpBuf[256] = {0};
+   char timest[25] = {0};
+   int check = 0;
+
+   /* register logger */
+   assert(logc_registerLogger(0x0011, FILEOUT, LOG_BASIC) == LOG_ERR_OK);
    assert(getLoggerCount() == 1);
+   /* set filename to log in */
+   assert(logc_setLogfile(0x0011, "unittests.log") == LOG_ERR_OK);
 
    /* tests of error format */
    assert(logc_setLogFormat(0x0011, ERR, CLEAN) == LOG_ERR_OK);
@@ -124,17 +118,57 @@ check_logFormat(
    assert(logc_logError(0x0011, 0x00000001, "error message") == LOG_ERR_OK);
    assert(logc_setLogFormat(0x0011, TIMESTAMP_ERR_TAG, CLEAN) == LOG_ERR_OK);
    assert(logc_logError(0x0011, 0x00000001, "error message") == LOG_ERR_OK);
-
    /* tests of log format */
    assert(logc_setLogFormat(0x0011, ERR, CLEAN) == LOG_ERR_OK);
    assert(logc_log(0x0011, LOG_BASIC, "log message") == LOG_ERR_OK);
    assert(logc_setLogFormat(0x0011, ERR, TIMESTAMP) == LOG_ERR_OK);
    assert(logc_log(0x0011, LOG_BASIC, "log message") == LOG_ERR_OK);
-
-
-
+   /* remove the logger to close the file */
    assert(logc_removeLogger(0x0011) == LOG_ERR_OK);
    assert(getLoggerCount() == 0);
+
+   /* get the timestamp and replace the seconds to compare the output */
+   addTimestamp(timest);
+   memset(timest+17, 'X', 2);
+   timest[24] = '\0';
+
+   /* open file to check the logging output */
+   fd = fopen("unittests.log", "r");
+   if (fd == NULL) {
+      printf("error couldn't open file\n");
+   }
+   /* check the output of the loggers */
+   assert(fgets(checkBuf, sizeof(checkBuf), fd) != NULL);
+   check = strncmp(checkBuf, "ERR : error message\n", sizeof(checkBuf));
+   assert(check == 0);
+
+   assert(fgets(checkBuf, sizeof(checkBuf), fd) != NULL);
+   check = strncmp(checkBuf, "ERR 0x00000001 : error message\n", sizeof(checkBuf));
+   assert(check == 0);
+
+   assert(fgets(checkBuf, sizeof(checkBuf), fd) != NULL);
+   memset(checkBuf+32, 'X', 2);
+   sprintf(tmpBuf, "ERR 0x00000001 %s : error message\n", timest);
+   check = strncmp(checkBuf, tmpBuf, sizeof(checkBuf));
+   assert(check == 0);
+
+   assert(fgets(checkBuf, sizeof(checkBuf), fd) != NULL);
+   memset(checkBuf+21, 'X', 2);
+   sprintf(tmpBuf, "ERR %s 0x00000001 : error message\n", timest);
+   check = strncmp(checkBuf, tmpBuf, sizeof(checkBuf));
+   assert(check == 0);
+
+   assert(fgets(checkBuf, sizeof(checkBuf), fd) != NULL);
+   memset(checkBuf+17, 'X', 2);
+   sprintf(tmpBuf, "%s ERR 0x00000001 : error message\n", timest);
+   check = strncmp(checkBuf, tmpBuf, sizeof(checkBuf));
+   assert(check == 0);
+
+   if (fd) {
+      fclose(fd);
+   }
+
+   remove("unittests.log");
 }
 
 
