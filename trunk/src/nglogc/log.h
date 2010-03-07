97a2d195c5b173cf3917a4e909d77a2d2fa8e4f4
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-03-07 10:14:58 +0000

    added defines guards for function macro.

--- trunk/src/nglogc/log.h
+++ trunk/src/nglogc/log.h
@@ -16,6 +16,14 @@
 
 /* =========== DEFINES ===================================================== */
 
+#if __STDC_VERSION__ < 199901L
+#if __GNUC__ >= 2
+#define __func__ __FUNCTION__
+#else
+#define __func__ "<unknown>"
+#endif
+#endif
+
 /*---------------------------------------------------------------------------*/
 /*
  * prints error messages to given logger and log level
