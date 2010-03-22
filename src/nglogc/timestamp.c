fde49132715552103b40144de6c103c21bd07765
ebrosius79 <ebrosius79@9cca1192-0654-11df-956f-01f849d29158>
2010-03-22 07:28:42 +0000

    added checks for struct tm and strftime().

--- src/nglogc/timestamp.c
+++ src/nglogc/timestamp.c
@@ -33,7 +33,16 @@
 
 #include "timestamp.h"
 
+#if defined TM_IN_SYS_TIME
+#include <sys/time.h>
+#endif
+
+#if defined (HAVE_TIME_H) && defined (HAVE_STRFTIME)
 #include <time.h>
+#else
+#define NO_TIMESTAMP
+#include <stdio.h>
+#endif
 
 /* =========== MODULE CONFIGURATION ======================================== */
 /* =========== DEFINES ===================================================== */
@@ -47,13 +56,20 @@ addTimestamp(
       char* str
       )
 {
+#ifndef NO_TIMESTAMP
    struct tm tim;
    time_t now;
+#endif
 
    if (str != NULL) {
+#ifndef NO_TIMESTAMP
       now = time(NULL);
       tim = *(localtime(&now));
       strftime(str, TIMEST_LENGTH, "%c", &tim);
+#else
+      const char* noTm = "<no timestamp on host>  ";
+      sprintf(str, "%s", noTm);
+#endif
       str[TIMEST_LENGTH-1] = 0x20;
    }
 }
